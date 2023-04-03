7dc310f7d2b16dc0c874c8fbd4465f445ca89475
ramon <rcanelav@gmail.com>
2023-04-03 19:22:15 +0200

    ADD: infinite scroll

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,6 +1,7 @@
 import { Button } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import InfiniteScroll from 'react-infinite-scroll-component';
 import { useSearchParams } from 'react-router-dom';
 import styled from 'styled-components';
 import { PostTitle } from '../components/postslist/PostTitle';
@@ -9,131 +10,52 @@ const { REACT_APP_API_URL } = process.env;
 export const Main = () => {
   const [searchParams ] = useSearchParams();
   const [ data, setData ] = useState([]);
+  const [ posts, setPosts ] = useState([]);
   const searchValue = searchParams.get('q');
+  const [ isLoading, setIsLoading ] = useState(true);
+  const [ page, setPage ] = useState(1);
+
   useEffect(() => {
     async function fetchData() {
-      const response = await axios.get(`${REACT_APP_API_URL}/api/v1/search?q=${searchValue || ''}`);
-      setData(response.data)
+      const response = await axios.get(`${REACT_APP_API_URL}/api/v1/search?q=${searchValue || ''}&page=${page}`);
+      setData( response.data);
+      setPosts( prev => (
+        [ ...prev, ...response.data.results]
+      ))
+      setIsLoading(false);
     }
     fetchData();
-  }, [ searchValue ]);
-  
-  const handleFirstPage = async() => {
-    if( !data.previous ) return;
-    const response = await axios.get(`${REACT_APP_API_URL}/api/v1/search?q=${searchValue || ''}&page=1`);
-    setData(response.data)
-  };
-  const handleLastPage = async() => {
-    if( !data.next ) return;
-    const response = await axios.get(`${REACT_APP_API_URL}/api/v1/search?q=${searchValue || ''}&page=${ data.totalPages }`);
-    setData(response.data)
-  };
-  const handleNextPage = async () => {
-    if(!data.next) return;
-    const response = await axios.get(`${REACT_APP_API_URL}/api/v1/search?q=${searchValue || ''}&page=${data.next.page}`);
-    setData(response.data)
-  };
-  const handlePreviousPage = async () => {
-    if(!data.previous) return;
-    const response = await axios.get(`${REACT_APP_API_URL}/api/v1/search?q=${searchValue || ''}&page=${data.previous.page}`);
-    setData(response.data)
-  };
-
+  }, [ searchValue, page ]);
+  if( isLoading ) return <div> is loading </div>
   return (
     <>
-      <StyledPostsWrapper>
-        {
-          data.results?.map(item => (
-            <PostTitle
-              key={item.id}
-              title={item.title}
-              date={item.postedAt}
-              author={`${item.userName} ${item.userLastname}`}
-              userImage={item.userImage}
-              userId={item.postedBy}
-              content={item.content}
-            />
-          ))
-        }
-      </StyledPostsWrapper>
-        <StyledButtonsContainer>
-          <StyledEndsButton onClick={ handleFirstPage } disabled={!data.previous}>First</StyledEndsButton>
-          <StyledPreviousPageButton
-                    onClick={ handlePreviousPage } disabled={!data.previous}>
-            Previous
-          </StyledPreviousPageButton>
-          <StyledNextPageButton
-                    onClick={handleNextPage} disabled={!data.next}>
-            Next
-          </StyledNextPageButton>
-          <StyledEndsButton
-                    onClick={ handleLastPage } disabled={!data.next}>
-            Last
-          </StyledEndsButton>
-
-        </StyledButtonsContainer>
+      <InfiniteScroll
+      dataLength={data.totalResults}
+      next={ () => setPage( prev => prev + 1 ) }
+      hasMore={ !!data.next}
+      >
+        <StyledPostsWrapper>
+          {
+            posts?.map(item => (
+              <PostTitle
+                key={item.id}
+                title={item.title}
+                date={item.postedAt}
+                author={`${item.userName} ${item.userLastname}`}
+                userImage={item.userImage}
+                userId={item.postedBy}
+                content={item.content}
+              />
+            ))
+          }
+        </StyledPostsWrapper>
+      </InfiniteScroll>
     </>
   );
 };
 
-const StyledButtonsContainer = styled.div`
-  display: flex;
-  justify-content: space-between;
-  margin: 1em 0;
-  width: 92%;
-  margin: 0 auto 2vh auto;
-  & > button {
-    font-weight: bold;
-  }
-  & > button:disabled {
-    background-color: gray;
-    color: white;
-  }
-
-  & > button:not(:disabled):hover {
-    background-color: black;
-    color: white;
-  }
-  @media (min-width: 768px) {
-    max-width: 50%;
-    min-width: 50%;
-
-    & > button {
-      font-size: 1.5em;
-      padding: 0.5em 1em;
-    }
-  }
-`;
-const StyledEndsButton = styled(Button)`
-  && {
-    margin: 1em 0;
-    width: 22%;
-    background-color: black;
-    color: yellow;
-  }
-`;
-
-const StyledNextPageButton = styled(Button)`
-  && {
-    margin: 1em 0;
-    width: 22%;
-    background-color: yellow;
-    color: black;
-    font-weight: bold;
-  }
-`;
-
-const StyledPreviousPageButton = styled(Button)`
-  && {
-    margin: 1em 0;
-    width: 22%;
-    background-color: yellow;
-    color: black;
-  }
-`;
-
 const StyledPostsWrapper = styled.div`
-  margin: 0 auto;
+  margin: 500px auto;
   max-width: 90%;
   min-width: 90%;
 
