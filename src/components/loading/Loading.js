371b36dde5e0ea4b702988869456051049cd77de
ramon <rcanelav@gmail.com>
2023-04-19 12:57:25 +0200

    ADD: Loading component

---
+++ src/components/loading/Loading.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import styled from 'styled-components';
+import UseAnimations from 'react-useanimations'
+import loading2 from 'react-useanimations/lib/loading2'
+
+export const Loading = () => {
+  return (
+    <LoadingWrapper>
+        <UseAnimations
+            size={100}
+            animation={loading2}
+            strokeColor='rgb(239, 255, 253)'
+            fillColor='rgba(255, 204, 3, 1)'
+        />
+    </LoadingWrapper>
+  )
+}
+
+const LoadingWrapper = styled.div`
+  & > * {
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+  }
+`;
