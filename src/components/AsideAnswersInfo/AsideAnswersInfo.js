933ce08309e2609f9c542f483b1514c707f77229
ramon <rcanelav@gmail.com>
2023-04-25 22:45:26 +0200

    UPDATE: aside display

--- src/components/AsideAnswersInfo/AsideAnswersInfo.js
+++ src/components/AsideAnswersInfo/AsideAnswersInfo.js
@@ -41,13 +41,12 @@ export const AsideAnswersInfo = ({children, url }) => {
 }
 
 const Wrapper = styled.div`
-    visibility: ${({posts}) => posts ? 'visible' : 'hidden'};
+    display: ${({posts}) => posts ? 'flex' : 'none'};
     background-color: rgba(255, 255, 255, 0.95);
     box-shadow: rgba(50, 50, 93, 0.25) 0px 2px 5px -1px, rgba(0, 0, 0, 0.3) 0px 1px 3px -1px;
     border-radius: 10px;
     margin: 1.5em 0.5em;
     flex: 0 1 100%;
-    display: flex;
     flex-flow: row wrap;
     max-height: 22.35em;
     word-break: break-all;
