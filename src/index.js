3ab7c8d1bb1cbd4aab65e36866f6ebe1b6fef6a8
ramon <rcanelav@gmail.com>
2023-05-01 15:03:56 +0200

    ADD: default axios baseurl

--- src/index.js
+++ src/index.js
@@ -1,8 +1,11 @@
+import axios from 'axios';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import { AuthProvider } from './hooks/useAuthorization';
+
+axios.defaults.baseURL = process.env.REACT_APP_API_URL;
 
 ReactDOM.render(
   <React.StrictMode>
