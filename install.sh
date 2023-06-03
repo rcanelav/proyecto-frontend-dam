f908daa964c4d06cf442db3a16fd8699550dc56a
Ramón Canela Villamizar <rcanelav@gmail.com>
2023-06-03 20:28:04 +0200

    ADD: installation script

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+# Install all the dependencies for the project
+npm install --legacy-peer-deps
+
+# Create .env file if it doesn't exist
+if [ ! -f ".env" ]; then
+    touch .env
+    # Append the env schema
+    echo "Generating the .env file..."
+    cat <<EOF >> .env
+PORT=3000
+REACT_APP_API_URL=http://localhost:3005
+EOF
+fi
