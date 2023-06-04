4d98ef66a9fa46f8bd70aa505b0a7b6667ae541c
Ramón Canela Villamizar <rcanelav@gmail.com>
2023-06-04 18:42:35 +0200

    UPDATE: tf bucket policies

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,6 +12,7 @@ data "aws_iam_policy_document" "frontend-bucket-policy" {
     sid = "1"
     actions = [
       "s3:GetObject",
+      "s3:GetObjectVersion",
     ]
     resources = [
       "${aws_s3_bucket.frontend-bucket.arn}",
@@ -24,3 +25,7 @@ data "aws_iam_policy_document" "frontend-bucket-policy" {
   }
 }
 
+resource "aws_s3_bucket_policy" "frontend-bucket-policy" {
+  bucket = "${aws_s3_bucket.frontend-bucket.id}"
+  policy = "${data.aws_iam_policy_document.frontend-bucket-policy.json}"
+}
